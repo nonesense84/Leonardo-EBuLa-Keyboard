bedda4f12b3a044a2e0697d278a101001937bb5c
nonesense84 <nonesense@freenet.de>
2021-10-25 18:36:04 +0200

    Improved control of Zusi. Added control for monitor buttons to use OSD. Changed comments to German.

--- Leonardo-EBuLa-Keyboard.ino
+++ Leonardo-EBuLa-Keyboard.ino
@@ -1,8 +1,38 @@
+/*
+ * Mit diesem Code kann ein Arduino Leonardo an einer Gercom EBuLa -Tastatur betrieben werden.
+ * Die Matrixtastatur wird gemaeß Anschluss-EBuLa-Leonardo.png an den Arduino angeschlossen. 
+ * Dieser Code bietet neben der Bedienung der EbuLa-Simulation von Zusi-Display auch die
+ * Moeglichkeit Zusi und auch den Monitor zu bedienen.
+ * Zur Bedienung von Zusi muss dessen Fenster im Focus von Windows sein. Unmittelbar dahinter
+ * muss Zusi-Display befinden.
+ * Auf diese Art und Weise kann durch die Uebertragung der Tastenkombination Alt + Tab zwischen
+ * den beiden Fenstern umgeschaltet werden.
+ * Durch druecken der EBuLa-Taste s wird der Arduino in den EBuLa-Modus geschaltet.
+ * Durch druecken der EBuLa-Taste i wird der Arduino in den Zusi-Modus geschaltet.
+ * Im Zusi-Modus werden die unteren Tasten 1 bis 0 als F1 bis F10 an Zusi gesendet.
+ * Die Pfeiltasten dienen zum Umschalten der Ansichten im Fuehrerstand.
+ * 
+ * Unter Verwendung von Optokopplern oder Relais kann der Arduino auch an die Bedientasten des
+ * verwendeten Monitors angeschlossen werden. Hiermit wird es moeglich Einstellungen im OSD des
+ * Monitors vorzunehmen. Dies ist insbesondere fuer die Helligkeitseinstellung praktisch.
+ * Fuer die Ansteuerung der Monitortasten sind die Anschluesse A0 bis A5 vorgesehen.
+ * Der Code ist Kassenmonitore der Marke TVS vorgesehen. Monitore von Siemens/Wincor/Nixdorf
+ * sind in der Regel baugleich. Fuer andere Geraete muesste der Code entsprechend angepasst werden.
+ * Nach druecken von St werdend die Tasten auf der rechten Seute an den Monitor geleitet.
+ * Druecken der Helligkeitstaste navigiert zusaetzlich direkt ins Helligkeitsmenue des Monitor OSD.
+ * Wer die Funktion nutzen moechte, muss die '//' in Zeile 33 entfernen.
+ */
+
 #include <Keyboard.h>
 #include <Keypad.h>
-bool toggleWindowBeforeKey = false;
-const byte ROWS = 5; // Five rows
-const byte COLS = 5; // Five columns
+#include <Mouse.h>
+byte mode = 0;
+#define mode_zusi 0
+#define mode_zusiDisplay  1
+#define mode_monitorOsd  2
+//#define MonitorControl
+const byte ROWS = 5; // Fuenf Zeilen
+const byte COLS = 5; // Fuenf Spalten
 // Define the Keymap
 char keys[ROWS][COLS] = {
   {'a','s','i','t','w'},
@@ -12,21 +42,27 @@ char keys[ROWS][COLS] = {
   {'5','4','3','2','1'}
 };
 
+byte rowPins[ROWS] = { 3, 5, 7, 9, 11 };  // Zeilen an diese Pins anschließen:
+byte colPins[COLS] = { 4, 6, 8, 10, 12 }; // Spalten an diese Pins anschließen:
 
-// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
-byte rowPins[ROWS] = { 3, 5, 7, 9, 11 };
-// Connect keypad COL0, COL1 and COL2 to these Arduino pins.
-byte colPins[COLS] = { 4, 6, 8, 10, 12 };
-
-// Create the Keypad
-Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
-#define ledpin 13
+Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );  // Keypad erzeugen
+#define ledpin 13    // LED wird an Pin 13 angeschlossen
 void setup()
-{
+{ 
+#ifdef MonitorControl            // Wenn Monitorbedientatsten verwendet werden...
+  pinMode(A0, OUTPUT);            // ...Ausgangspins festlegen
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(A4, OUTPUT);
+  pinMode(A5, OUTPUT);
+  digitalWrite(A0, LOW);          // Ist die Masse fuer die Optokoppler
+#endif
   pinMode(ledpin,OUTPUT);
   digitalWrite(ledpin, LOW);
   Serial.begin(9600);
   Keyboard.begin();
+  Mouse.begin();
 }
 
 void loop()
@@ -37,32 +73,97 @@ void loop()
     Serial.println(key);
     switch (key)
     {
-      case 's':       // Key S not used by Zusi-Display. Lets use it for Atl+Tab
-        sendAltTab(0);
+      case 's':       // Taste s wird nicht von Zusi-Display benutzt. Sie schaltet hier in den Zusi-Display-Modus
+        mode = mode_zusiDisplay;
         break;
-      case 'i':       // Key i not used by Zusi-Display. Lets use it to turn
-                      // on and off the window toggle mode.
-        switchWindowToggle();
+      case 'i':       // Taste i wird nicht von Zusi-Display benutzt. Sie schaltet hier in den Zusi-Modus
+        mode = mode_zusi;
         break;
-      case 't':       // Key St not used by Zusi-Display. Lets use it fast forward in zusi
-        pressAndRelease(KEY_F1);
+#ifdef MonitorControl
+      case 't':       // Taste St wird nicht von Zusi-Display benutzt. Hier wird sie benutzt, um in den Monitor OSD-Modus zu wechseln.
+        mode = mode_monitorOsd;
+        pushMonitorBtn('c');  // Nach wechsel in den OSD-Modus, das Menue direkt oeffnen.
         break;
-      case 'w':       // Key V>0 not used by Zusi-Display. Lets use it for a break in zusi
-        pressAndRelease(KEY_F2);
+      case 'h':       // Helligkeitstaste wird zwar von Zusi-Display benutzt...
+                      // ...aber es soll das Helligkeitsmenue des Monitors verwendet werden.
+        mode = mode_monitorOsd;
+        pushMonitorBtn('c');
+        delay(500);
+        pushMonitorBtn('e');
         break;
-      case 'v':       // Key St not used by Zusi-Display. Lets use it for time jump in zusi
-        pressAndRelease(KEY_F4);
+#endif
+      case 'w':       // Taste V>0 wird nicht von Zusi-Display benutzt.
+      case 'v':       // Taste St wird nicht von Zusi-Display benutzt.
         break;
       default:
-        sendKey(key);
+        if(mode == mode_zusiDisplay)  sendKey(key);
+        if(mode == mode_zusi)         sendZusiKey(key);
+#ifdef MonitorControl
+        if(mode == mode_monitorOsd)   pushMonitorBtn(key);
+#endif
     }
   }
 }
 void sendKey(char key){
   digitalWrite(ledpin, HIGH);
-  if(toggleWindowBeforeKey)sendAltTab(100);
+  sendAltTab(100);
   pressAndRelease(key);
-  if(toggleWindowBeforeKey)sendAltTab(0);
+  sendAltTab(0);
+  digitalWrite(ledpin, LOW);
+}
+void sendZusiKey(char key){
+  digitalWrite(ledpin, HIGH);
+    switch (key)
+    {
+      case '1':
+        pressAndRelease(KEY_F1);  // Zeitraffer
+        break;
+      case '2':
+        pressAndRelease(KEY_F2);  // Pause
+        break;
+      case '3':
+        pressAndRelease(KEY_F3);  // Autopilot
+        break;
+      case '4':
+        pressAndRelease(KEY_F4);  // Zeitsprung
+        break;
+      case '5':
+        pressAndRelease(KEY_F5);  // Fuehrerstand ein-/ausblenden
+        break;
+      case '6':
+        pressAndRelease(KEY_F6);  // Fuehrerstand-Zusatzinfo anzeigen
+        break;
+      case '7':
+        pressAndRelease(KEY_F7);  // Zugdaten einblenden
+        break;
+      case '8':
+        pressAndRelease(KEY_F8);  // Schummelinfo anzeigen
+        break;
+      case '9':
+        pressAndRelease(KEY_F9);  // Standort Fuehrerstand
+        break;
+      case '0':
+        pressAndRelease(KEY_F12); // Standort Zug-Aussenansicht hinten
+        Mouse.press();            // Damit sich der Blick um das Zugende dreht...
+        Mouse.move(50, 0, 0);     // ...muss ein Mausklick mit Bewegung gemacht werden.
+        Mouse.release();
+        Mouse.move(-50, 0, 0);
+        break;
+      case 'u':
+        pressAndRelease(KEY_UP_ARROW);    // Pfleil hoch
+        break;
+      case 'd':
+        pressAndRelease(KEY_DOWN_ARROW);  // Pfleil runter
+        break;
+      case 'l':
+        pressAndRelease(KEY_LEFT_ARROW);  // Pfleil links
+        break;
+      case 'r':
+        pressAndRelease(KEY_RIGHT_ARROW); // Pfleil rechts
+        break;
+      default:
+        sendKey(key);
+    }
   digitalWrite(ledpin, LOW);
 }
 void pressAndRelease(char key){
@@ -70,22 +171,56 @@ void pressAndRelease(char key){
   delay(200);
   Keyboard.releaseAll();
 }
-void switchWindowToggle(){
-  digitalWrite(ledpin, HIGH);
-  delay(50);
-  digitalWrite(ledpin, LOW);
-  delay(100);
-  toggleWindowBeforeKey = !toggleWindowBeforeKey;
-  if(toggleWindowBeforeKey){
-    digitalWrite(ledpin, HIGH);
-    delay(50);
-    digitalWrite(ledpin, LOW);
-  }
-}
-
 void sendAltTab(byte waitMs){
   Keyboard.press(KEY_LEFT_ALT);
   Keyboard.press(KEY_TAB);
   Keyboard.releaseAll();
   delay(waitMs);
 }
+#ifdef MonitorControl
+void pushMonitorBtn(char key){
+  switch (key){
+    case 'a':                 // Taste An / Aus
+      digitalWrite(A1, HIGH);
+      delay(65);
+      digitalWrite(A1, LOW);
+      break;
+    case 'e':                 // Taste Enter
+      digitalWrite(A2, HIGH);
+      delay(65);
+      digitalWrite(A2, LOW);
+      break;
+    case 'c':                 // Taste Menue
+      digitalWrite(A3, HIGH);
+      delay(65);
+      digitalWrite(A3, LOW);
+      break;
+    case 'd':                 // Taste Hoch
+      digitalWrite(A4, HIGH);
+      delay(65);
+      digitalWrite(A4, LOW);
+      break;
+    case 'u':                 // Taste Runter
+      digitalWrite(A5, HIGH);
+      delay(65);
+      digitalWrite(A5, LOW);
+      break;
+    case 'l':                 // Taste Hoch 10 mal druecken
+      for(byte n = 0; n<10; n++){
+        digitalWrite(A4, HIGH);
+        delay(65);
+        digitalWrite(A4, LOW);
+        delay(65);
+      }
+      break;
+    case 'r':                 // Taste Runter 10 mal druecken
+      for(byte n = 0; n<10; n++){
+        digitalWrite(A5, HIGH);
+        delay(65);
+        digitalWrite(A5, LOW);
+        delay(65);
+      }
+  }
+  delay(25);
+}
+#endif
